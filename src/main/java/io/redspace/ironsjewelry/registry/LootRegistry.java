bfb97fb17fae1a3187013f49a315778d62c4766a
iron431 <34083081+iron431@users.noreply.github.com>
2024-10-13 16:54:35 -0500

    jewelry recipe loot modifiers

--- src/main/java/io/redspace/ironsjewelry/registry/LootRegistry.java
+++ src/main/java/io/redspace/ironsjewelry/registry/LootRegistry.java
@@ -1,21 +1,29 @@
 package io.redspace.ironsjewelry.registry;
 
+import com.mojang.serialization.MapCodec;
 import io.redspace.ironsjewelry.IronsJewelry;
+import io.redspace.ironsjewelry.loot.AppendLootModifier;
 import io.redspace.ironsjewelry.loot.SetHeldPatternLootFunction;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.storage.loot.functions.LootItemFunctionType;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.common.loot.IGlobalLootModifier;
 import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
 import java.util.function.Supplier;
 
 public class LootRegistry {
     private static final DeferredRegister<LootItemFunctionType<?>> LOOT_FUNCTIONS = DeferredRegister.create(Registries.LOOT_FUNCTION_TYPE, IronsJewelry.MODID);
+    private static final DeferredRegister<MapCodec<? extends IGlobalLootModifier>> LOOT_MODIFIER_SERIALIZERS = DeferredRegister.create(NeoForgeRegistries.Keys.GLOBAL_LOOT_MODIFIER_SERIALIZERS, IronsJewelry.MODID);
 
     public static final Supplier<LootItemFunctionType<?>> SET_HELD_PATTERN =
             LOOT_FUNCTIONS.register("set_held_pattern", () -> new LootItemFunctionType<>(SetHeldPatternLootFunction.CODEC));
 
+    public static final Supplier<MapCodec<? extends IGlobalLootModifier>> APPEND_LOOT_MODIFIER = LOOT_MODIFIER_SERIALIZERS.register("append_loot", AppendLootModifier.CODEC);
+
     public static void register(IEventBus modEventBus) {
         LOOT_FUNCTIONS.register(modEventBus);
+        LOOT_MODIFIER_SERIALIZERS.register(modEventBus);
     }
 }
