20972f1452f1da62092683ebd605ef1098bfbc01
iron431 <34083081+iron431@users.noreply.github.com>
2024-10-18 16:19:42 -0500

    generate jewelry loot function

--- src/main/java/io/redspace/ironsjewelry/loot/GenerateJewelryLootFunction.java
+++ src/main/java/io/redspace/ironsjewelry/loot/GenerateJewelryLootFunction.java
@@ -15,24 +15,16 @@ import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.functions.LootItemFunction;
 import net.minecraft.world.level.storage.loot.functions.LootItemFunctionType;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
-import java.util.TreeMap;
+import java.util.*;
 
 public record GenerateJewelryLootFunction(
-        Holder<JewelryType> type,
         HolderSet<PatternDefinition> patternSource,
-        double qualityMin, double qualityMax,
-        Optional<HolderSet<MaterialDefinition>> materialFilter
+        Optional<Map<String, HolderSet<MaterialDefinition>>> materialFilter
 
 ) implements LootItemFunction {
     public static MapCodec<GenerateJewelryLootFunction> CODEC = RecordCodecBuilder.mapCodec(builder -> builder.group(
-            IronsJewelryRegistries.JEWELRY_TYPE_REGISTRY.holderByNameCodec().fieldOf("type").forGetter(GenerateJewelryLootFunction::type),
             RegistryCodecs.homogeneousList(IronsJewelryRegistries.Keys.PATTERN_REGISTRY_KEY).fieldOf("patterns").forGetter(GenerateJewelryLootFunction::patternSource),
-            Codec.DOUBLE.fieldOf("qualityMin").forGetter(GenerateJewelryLootFunction::qualityMin),
-            Codec.DOUBLE.fieldOf("qualityMax").forGetter(GenerateJewelryLootFunction::qualityMax),
-            RegistryCodecs.homogeneousList(IronsJewelryRegistries.Keys.MATERIAL_REGISTRY_KEY).optionalFieldOf("materialFilter").forGetter(GenerateJewelryLootFunction::materialFilter)
+            Codec.unboundedMap(Codec.STRING, RegistryCodecs.homogeneousList(IronsJewelryRegistries.Keys.MATERIAL_REGISTRY_KEY)).optionalFieldOf("materials").forGetter(GenerateJewelryLootFunction::materialFilter)
 
     ).apply(builder, GenerateJewelryLootFunction::new));
 
@@ -44,35 +36,43 @@ public record GenerateJewelryLootFunction(
     @Override
     public ItemStack apply(ItemStack stack, LootContext lootContext) {
         TreeMap<Integer, Holder<PatternDefinition>> weightedPatterns = new TreeMap<>();
-        int total = 0;
-        for (int i = 0; i < this.patternSource.size(); i++) {
-            var pattern = this.patternSource.get(i);
-            if (pattern.value().jewelryType().equals(this.type.value())) {
-                weightedPatterns.put(total, pattern);
-                total += (int) (pattern.value().qualityMultiplier() * 100);
+        //Automatically restrict type from itemstack
+        var typeOpt = IronsJewelryRegistries.JEWELRY_TYPE_REGISTRY.stream().filter(jewelryType -> jewelryType.item().value().equals(stack.getItem())).findFirst();
+        if (typeOpt.isPresent()) {
+            int total = 0;
+            //build weighted map based inversely on the quality of the pattern (higher quality patterns have a lower chance to roll)
+            for (int i = 0; i < this.patternSource.size(); i++) {
+                var pattern = this.patternSource.get(i);
+                if (pattern.value().jewelryType().equals(typeOpt.get())) {
+                    weightedPatterns.put(total, pattern);
+                    total += (int) (100 / (1 + pattern.value().qualityMultiplier()));
+                }
+            }
+            if (!weightedPatterns.isEmpty()) {
+                var pattern = weightedPatterns.lowerEntry(lootContext.getRandom().nextInt(total)).getValue();
+                HashMap<Holder<PartDefinition>, Holder<MaterialDefinition>> materials = new HashMap<>();
+                var registry = IronsJewelryRegistries.materialRegistry(lootContext.getLevel().registryAccess());
+                for (PartIngredient part : pattern.value().partTemplate()) {
+                    // Find applicable materials by
+                    // a): the material can be used for this part
+                    // b): the material filter is empty, or the material filter does not specify this material type, or this material is specified by type in the material filter
+                    List<MaterialDefinition> applicableMaterials = registry.stream().filter(
+                            (material) ->
+                                    part.part().value().canUseMaterial(material.materialType()) &&
+                                            (materialFilter.isEmpty() ||
+                                                    material.materialType().stream().anyMatch(type -> !materialFilter.get().containsKey(type) || materialFilter.get().get(type).contains(registry.wrapAsHolder(material))))
+                    ).toList();
+                    if (!applicableMaterials.isEmpty()) {
+                        materials.put(part.part(), registry.wrapAsHolder(applicableMaterials.get(lootContext.getRandom().nextInt(applicableMaterials.size()))));
+                    }
+                }
+                var jewelryData = new JewelryData(pattern, materials);
+                if (jewelryData.isValid()) {
+                    stack.set(ComponentRegistry.JEWELRY_COMPONENT, jewelryData);
+                    return stack;
+                }
             }
         }
-        var pattern = weightedPatterns.lowerEntry(lootContext.getRandom().nextInt(total)).getValue();
-        HashMap<Holder<PartDefinition>, Holder<MaterialDefinition>> materials = new HashMap<>();
-        var registry = IronsJewelryRegistries.materialRegistry(lootContext.getLevel().registryAccess());
-        for (PartIngredient part : pattern.value().partTemplate()) {
-            List<MaterialDefinition> applicableMaterials = registry.stream().filter(
-                    (material) -> material.quality() >= this.qualityMin && material.quality() <= this.qualityMax &&
-                            part.part().value().canUseMaterial(material.materialType()) &&
-                            //Fixme: if you wanted to limit say, only metal, this would require you to list all gem types now
-                            // could just literally save this as a map of strings to holderset instead
-                            (materialFilter.isEmpty() || materialFilter.get().contains(registry.wrapAsHolder(material)))
-            ).toList();
-            if (!applicableMaterials.isEmpty()) {
-                materials.put(part.part(), registry.wrapAsHolder(applicableMaterials.get(lootContext.getRandom().nextInt(applicableMaterials.size()))));
-            }
-        }
-        var jewelryData = new JewelryData(pattern, materials);
-        if (jewelryData.isValid()) {
-            stack.set(ComponentRegistry.JEWELRY_COMPONENT, jewelryData);
-            return stack;
-        } else {
-            return ItemStack.EMPTY;
-        }
+        return ItemStack.EMPTY;
     }
 }
