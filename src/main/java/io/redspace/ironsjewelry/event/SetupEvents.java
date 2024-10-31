cfaa7bb89541d48b64a5fcf6d250a08af325fadb
iron431 <34083081+iron431@users.noreply.github.com>
2024-10-31 17:09:26 -0500

    add gem trades

--- src/main/java/io/redspace/ironsjewelry/event/SetupEvents.java
+++ src/main/java/io/redspace/ironsjewelry/event/SetupEvents.java
@@ -2,6 +2,7 @@ package io.redspace.ironsjewelry.event;
 
 import io.redspace.ironsjewelry.IronsJewelry;
 import io.redspace.ironsjewelry.core.Trades;
+import io.redspace.ironsjewelry.registry.ItemRegistry;
 import io.redspace.ironsjewelry.registry.VillagerRegistry;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
@@ -32,13 +33,20 @@ public class SetupEvents {
             Apprentice
              */
             apprentice.add(new Trades.BuyItem(Items.DIAMOND, 1, 1, 12, 10, 0.05F));
-            apprentice.add(new Trades.BuyItem(Items.AMETHYST_SHARD, 2, 1, 12, 10, 0.05F));
-            apprentice.add(new Trades.BuyItem(Items.LAPIS_LAZULI, 6, 2, 12, 8, 0.05F));
-            apprentice.add(new Trades.SellLootTable(ResourceKey.create(Registries.LOOT_TABLE, IronsJewelry.id("trades/sell_jewelry")), 2, 15, 0.5f, Trades::calculateJewelryPrice));
+            apprentice.add(new Trades.BuyItem(Items.AMETHYST_SHARD, 3, 1, 12, 10, 0.05F));
+            apprentice.add(new Trades.BuyItem(Items.LAPIS_LAZULI, 6, 1, 12, 8, 0.05F));
+            apprentice.add(new Trades.BuyItem(ItemRegistry.RUBY.get(), 4, 1, 12, 10, 0.05F));
+            apprentice.add(new Trades.BuyItem(ItemRegistry.SAPPHIRE.get(), 4, 1, 12, 10, 0.05F));
+            apprentice.add(new Trades.BuyItem(ItemRegistry.TOPAZ.get(), 4, 1, 12, 10, 0.05F));
+            apprentice.add(new Trades.BuyItem(ItemRegistry.MOONSTONE.get(), 4, 1, 12, 10, 0.05F));
+            apprentice.add(new Trades.BuyItem(ItemRegistry.PERIDOT.get(), 4, 1, 12, 10, 0.05F));
+            apprentice.add(new Trades.BuyItem(ItemRegistry.ONYX.get(), 4, 1, 12, 10, 0.05F));
+
             /*
             Journeyman
              */
             journeyman.add(new Trades.SellLootTable(ResourceKey.create(Registries.LOOT_TABLE, IronsJewelry.id("trades/sell_basic_pattern")), 1, 25, 0.5f, Trades::calculatePatternPrice));
+            journeyman.add(new Trades.SellLootTable(ResourceKey.create(Registries.LOOT_TABLE, IronsJewelry.id("trades/sell_jewelry")), 2, 15, 0.5f, Trades::calculateJewelryPrice));
             /*
             Expert
              */
