81dbfe3d942a4a1d7d1568c910c20c50f832c1cf
iron431 <34083081+iron431@users.noreply.github.com>
2024-11-23 15:43:53 -0600

    update atlas api

--- src/main/java/io/redspace/ironsjewelry/registry/AssetHandlerRegistry.java
+++ src/main/java/io/redspace/ironsjewelry/registry/AssetHandlerRegistry.java
@@ -1,6 +1,7 @@
 package io.redspace.ironsjewelry.registry;
 
 import io.redspace.atlasapi.api.AssetHandler;
+import io.redspace.atlasapi.api.AtlasApiRegistry;
 import io.redspace.ironsjewelry.IronsJewelry;
 import io.redspace.ironsjewelry.client.JewelryAssetHandler;
 import net.neoforged.bus.api.IEventBus;
@@ -9,7 +10,7 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 import java.util.function.Supplier;
 
 public class AssetHandlerRegistry {
-    private static final DeferredRegister<AssetHandler> HANDLERS = DeferredRegister.create(io.redspace.atlasapi.api.AssetHandlerRegistry.ASSET_HANDLER_REGISTRY_KEY, IronsJewelry.MODID);
+    private static final DeferredRegister<AssetHandler> HANDLERS = DeferredRegister.create(AtlasApiRegistry.ASSET_HANDLER_REGISTRY_KEY, IronsJewelry.MODID);
 
     public static final Supplier<JewelryAssetHandler> JEWELRY_HANDLER = HANDLERS.register("jewelry", JewelryAssetHandler::new);
 
