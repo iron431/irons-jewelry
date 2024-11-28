1c7ead4a7ee162ef890a4574a2bfb6441c17b12c
iron431 <34083081+iron431@users.noreply.github.com>
2024-11-27 23:27:54 -0600

    on_attack -> on_attack_bonus

--- src/main/java/io/redspace/ironsjewelry/registry/BonusTypeRegistry.java
+++ src/main/java/io/redspace/ironsjewelry/registry/BonusTypeRegistry.java
@@ -16,7 +16,7 @@ public class BonusTypeRegistry {
     public static final Supplier<BonusType> ATTRIBUTE_BONUS = BONUSES.register("attribute_bonus", AttributeBonusType::new);
     public static final Supplier<BonusType> EFFECT_ON_HIT_BONUS = BONUSES.register("effect_on_hit_bonus", EffectOnHitBonusType::new);
     public static final Supplier<OnProjectileHitBonusType> ON_PROJECTILE_HIT_BONUS = BONUSES.register("on_projectile_hit_bonus", OnProjectileHitBonusType::new);
-    public static final Supplier<OnAttackBonusType> ON_ATTACK_BONUS = BONUSES.register("on_attack", OnAttackBonusType::new);
+    public static final Supplier<OnAttackBonusType> ON_ATTACK_BONUS = BONUSES.register("on_attack_bonus", OnAttackBonusType::new);
     public static final Supplier<BonusType> EFFECT_IMMUNITY_BONUS = BONUSES.register("effect_immunity_bonus", EffectImmunityBonusType::new);
     public static final Supplier<OnShieldBlockBonusType> ON_SHIELD_BLOCK_BONUS = BONUSES.register("on_shield_block_bonus", OnShieldBlockBonusType::new);
     public static final Supplier<OnTakeDamageBonusType> ON_TAKE_DAMAGE_BONUS = BONUSES.register("on_take_damage_bonus", OnTakeDamageBonusType::new);
