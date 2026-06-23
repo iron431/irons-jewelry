2a8ea0d7b66654dbd54472aff576cf2e37fc24bd
iron431 <34083081+iron431@users.noreply.github.com>
2026-06-22 22:37:02 -0500

    bump version

--- src/main/java/io/redspace/ironsjewelry/core/data/JewelryType.java
+++ src/main/java/io/redspace/ironsjewelry/core/data/JewelryType.java
@@ -3,13 +3,14 @@ package io.redspace.ironsjewelry.core.data;
 import net.minecraft.core.Holder;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.neoforged.fml.loading.FMLLoader;
 import top.theillusivec4.curios.api.CuriosSlotTypes;
 
 import java.util.Optional;
 
 public record JewelryType(Holder<Item> item) {
     public Optional<String> getCuriosSlotIdentifier() {
-        var slotTypes = CuriosSlotTypes.getItemSlotTypes(new ItemStack(item.value()), false);
+        var slotTypes = CuriosSlotTypes.getItemSlotTypes(new ItemStack(item.value()), FMLLoader.getCurrent().getDist().isClient());
         return slotTypes.keySet().stream().findFirst();
     }
 }
